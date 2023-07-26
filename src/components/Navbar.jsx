ed689fce62df9c3346f5c9c12c931a78e35139fc
alanbacay02 <alan.social02@gmail.com>
2023-07-26 15:34:06 +0800

    Adds component MountainSVG.jsx that returns an SVG element and imports it as a logo in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import { CiUser } from 'react-icons/ci';
 import { SlMenu } from 'react-icons/sl';
-import NavLogo from '../assets/vecteezy_mountain_logo.svg';
+import MountainSVG from './MountainSVG.jsx';
 import PopoutMenu from './PopoutMenu.jsx';
 
 
@@ -16,9 +16,10 @@ const Navbar = () => {
   return (
     <div className='absolute z-50 top-0 left-0 right-0 flex flex-row justify-between items-center h-16 sm:h-20 md:h-24 w-full mx-auto px-2 md:px-6 text-white'>
       <div className='flex flex-row items-center justify-center'>
-        <img className='fill-white h-[50px] w-[50px]' src={NavLogo} alt='vecteezy_mountain.svg' />
-        <h1 className='text-lg sm:text-2xl md:text-3xl font-bold hover:cursor-pointer'>HIGH<span className='text-[#56ace1]'>LANDER</span>.</h1>
-        {/* <a href="https://www.vecteezy.com/free-vector/mountain-icon">Mountain Icon Vectors by Vecteezy</a> */}
+        <div className='w-[45px] h-full sm:w-[70px] sm:h-[70px] fill-white'>
+          <MountainSVG />
+        </div>
+        <h1 className='text-lg sm:text-2xl md:text-3xl font-bold hover:cursor-pointer'>HIGH<span className='text-[#000000]'>LANDER</span>.</h1>
       </div>
       <div>
         <div className='flex w-full flex-row justify-center items-center'>
