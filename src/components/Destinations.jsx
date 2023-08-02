67e2dc426ab6eb7d16bba5d7eeb255a9782c3077
alanbacay02 <alan.social02@gmail.com>
2023-08-02 17:54:31 +0800

    Adds slanted ribbon to p element in Destinations.jsx

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -7,7 +7,9 @@ const Destinations = () => {
     <div className='relative w-full py-16 px-6'>
       <div className='w-full max-w-[1300px] mx-auto'>
         <div className='flex flex-col justify-center items-center text-center p-2'>
-          <p className='font-medium text-base md:text-lg text-white bg-[#56ace1] py-1 px-2'>FIND YOUR NEXT GETAWAY</p>
+          <div className='slanted-container'>
+            <p className='slanted-text font-medium text-base md:text-lg text-white bg-blue-500 py-1'>FIND YOUR NEXT GETAWAY</p>
+          </div>
           <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl mt-3'>Popular Destinations Right Now</h1>
         </div>
         <div className='relative my-3'>
