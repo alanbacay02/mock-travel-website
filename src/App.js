aa125fdb24f2629c4c7342e1af03f92745dbaf16
alanbacay02 <alan.social02@gmail.com>
2023-08-07 16:54:42 +0800

    Creates a component Newsletter.jsx and imports it to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Destinations from './components/Destinations';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from "./components/Navbar";
+import Newsletter from './components/Newsletter';
 import ValueProposition from './components/ValueProposition';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Destinations />
       <ValueProposition />
+      <Newsletter />
       <Footer />
     </div>
   );
