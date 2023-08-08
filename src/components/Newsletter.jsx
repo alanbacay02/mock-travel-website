d9f70f4c0ac7184e951fb45db2ef943824c677e6
alanbacay02 <alan.social02@gmail.com>
2023-08-08 14:59:34 +0800

    Finalizes design for Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div className='w-full py-12 bg-blue-800'>
-      <div className='max-w-[1240px] grid md:grid-cols-2 mx-auto px-6 items-center'>
-        <div className='text-white'>
-          <h1 className='font-bold text-2xl sm:text-2xl md:text-3xl'>Don't Miss Out on Your Dream Getaway!</h1>
-          <p className='text-sm sm:text-base md:text-lg'>Your dream vacation is a click away - receive the latest offers and destinations in your inbox.</p>
+    <div className='w-full py-12 bg-sky-400 text-black'>
+      <div className='max-w-[1240px] grid md:grid-cols-2 gap-x-4 mx-auto px-6 items-center'>
+        <div>
+          <h1 className='font-bold text-2xl sm:text-2xl md:text-3xl mb-2 text-center md:text-left'>Don't Miss Out on Your Dream Getaway!</h1>
+          <p className='text-sm sm:text-base md:text-lg text-center md:text-left'>Your dream vacation is a click away - receive the latest offers and destinations in your inbox.</p>
         </div>
         <div className='my-4'>
           <form className='w-full' onSubmit={() => {}}>
-            <div className='flex flex-col gap-x-2 gap-y-3 sm:flex-row items-center w-full'>
+            <div className='flex flex-col gap-x-2 gap-y-3 sm:flex-row items-center w-full justify-center'>
               <label htmlFor='email' className='sr-only'>Email</label>
               <input
-                className='rounded-lg px-4 py-2 w-full outline-none'
+                className='rounded-lg px-4 py-2 w-full max-w-[450px] outline-none'
                 placeholder='Enter Email'
               />
-              <button className='bg-zinc-950 text-white w-44 px-4 py-2 rounded-lg'>Notify Me</button>
+              <button className='bg-[#fe9000] w-44 px-4 py-2 rounded-lg'>Notify Me</button>
             </div>
           </form>
-          <p className='text-white text-center md:text-left my-1'>We care about the protection of your data. Read our <a href='/#' className='text-yellow-400 underline'>privacy policy</a>.</p>
+          <p className='text-center md:text-left my-1'>We care about the protection of your data. Read our <a href='/#' className='underline'>privacy policy</a>.</p>
         </div>
       </div>
     </div>
