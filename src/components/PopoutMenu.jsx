64e115aecd319d48b563a56cd8e2c7f3e631332b
alanbacay02 <alan.social02@gmail.com>
2023-08-08 17:33:41 +0800

    fixes bug where transition does not work when popout menu is closed on mobile devices. Fixes responsiveness of popout menu for small devices

--- src/components/PopoutMenu.jsx
+++ src/components/PopoutMenu.jsx
@@ -46,42 +46,42 @@ const PopoutMenu = (props) => {
         ref={menuRef} 
         className={ props.navMenu
           ? 'fixed h-screen top-0 right-0 w-[85%] bg-white text-black ease-in-out duration-500 shadow-2xl md:block md:absolute md:h-fit md:right-4 md:top-[80px] md:w-[300px] md:rounded-lg md:transition-none z-10'
-          : 'right-[-100%] fixed h-screen top-0 w-[85%] bg-white text-black ease-in-out duration-500 md:transition-none'
+          : 'right-[-100%] fixed h-screen top-0 w-[85%] bg-white text-black ease-in-out duration-500 md:transition-none z-10'
         }
       >
-        <ul className='flex flex-col h-fit'>
-          <li className="flex justify-end p-4 md:hidden">
-            <AiOutlineClose onClick={() => {props.handleNavMenu()}} size={25} />
+        <ul className='flex flex-col h-fit text-base sm:text-lg'>
+          <li className="flex justify-end p-4 md:hidden text-xl">
+            <AiOutlineClose onClick={() => {props.handleNavMenu()}} />
           </li>
           <li className="p-4 font-medium inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <AiOutlineInfoCircle className='my-auto mr-5 text-blue-600' size={20} />
+            <AiOutlineInfoCircle className='my-auto mr-5 text-blue-600' />
             About
           </li>
           <li className="p-4 font-medium inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <BsGlobeAmericas className='my-auto mr-5 text-blue-600' size={20} />
+            <BsGlobeAmericas className='my-auto mr-5 text-blue-600' />
             Destinations
           </li>
           <li className="p-4 font-medium inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <BiPackage className='my-auto mr-5 text-blue-600' size={20} />
+            <BiPackage className='my-auto mr-5 text-blue-600' />
             Packages
           </li>
           <li className="p-4 font-medium border-b inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <BsClipboardCheck className='my-auto mr-5 text-blue-600' size={20} />
+            <BsClipboardCheck className='my-auto mr-5 text-blue-600' />
             Reviews
           </li>
           <li className="p-4 font-medium inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <ImPriceTag className='my-auto mr-5 text-blue-600' size={20} />
+            <ImPriceTag className='my-auto mr-5 text-blue-600' />
             Explore Everywhere
           </li>
           <li className="p-4 font-medium border-b inline-flex hover:cursor-pointer hover:bg-gray-200">
-            <AiOutlineQuestionCircle className='my-auto mr-5 text-blue-600' size={20} /> 
+            <AiOutlineQuestionCircle className='my-auto mr-5 text-blue-600' /> 
             Help
           </li>
-          <li className='flex flex-row p-3 mx-auto my-auto space-x-5 md:space-x-8 '>
-            <FaFacebookSquare className='text-blue-600 hover:cursor-pointer' size={30}/>
-            <FaInstagram className='text-blue-600 hover:cursor-pointer' size={30} />
-            <FaTwitter className='text-blue-600 hover:cursor-pointer' size={30} />
-            <FaPinterestSquare className='text-blue-600 hover:cursor-pointer' size={30} />
+          <li className='flex flex-row p-3 mx-auto my-auto space-x-5 md:space-x-8 text-2xl xs:text-3xl'>
+            <FaFacebookSquare className='text-blue-600 hover:cursor-pointer active:text-blue-800' />
+            <FaInstagram className='text-blue-600 hover:cursor-pointer active:text-blue-800' />
+            <FaTwitter className='text-blue-600 hover:cursor-pointer active:text-blue-800' />
+            <FaPinterestSquare className='text-blue-600 hover:cursor-pointer active:text-blue-800' />
           </li>
         </ul>
       </div>
