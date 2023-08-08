ec9c3b1ef14007749203f17a3f5a1671f06a3bbd
alanbacay02 <alan.social02@gmail.com>
2023-08-08 16:39:35 +0800

    fixes icon responsiveness on small devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,10 +23,10 @@ const Navbar = () => {
     <div>
       <div id='navbar' className='absolute top-0 left-0 right-0 flex flex-row justify-between items-center h-16 sm:h-20 md:h-24 w-full mx-auto px-2 md:px-6 text-white z-10'>
         <div className='flex flex-row items-center justify-center'>
-          <div className='w-[45px] h-full sm:w-[70px] fill-white'>
+          <div className='w-[40px] xs:w-[50px] sm:w-[70px] h-full fill-white'>
             <MountainSVG />
           </div>
-          <h1 className='text-lg sm:text-2xl md:text-3xl font-bold hover:cursor-pointer'>HIGH<span className='text-[#56ace1]'>LANDER</span>.</h1>
+          <h1 className='xs:text-xl sm:text-2xl md:text-3xl font-bold hover:cursor-pointer'>HIGH<span className='text-[#56ace1]'>LANDER</span>.</h1>
         </div>
         <div>
           <div className='flex w-full flex-row justify-center items-center'>
@@ -34,8 +34,8 @@ const Navbar = () => {
             className='flex justify-center items-center p-[4px] sm:p-2 m-3 rounded-full bg-white text-black gap-1 hover:cursor-pointer'
             onClick={handleLoginMenu} 
             >
-              <CiUser size={20}/> 
-              <p className='font-medium text-xs sm:text-base pr-1'>Log in</p>
+              <CiUser className='text-xs xs:text-base sm:text-lg md:text-xl'/>
+              <p className='font-medium text-xs xs:text-base pr-1'>Log in</p>
             </div>
             <div className='block' onClick={handleNavMenu}>
               {navMenu ? <AiOutlineClose size={20} className='md:ml-2 hover:cursor-pointer' /> :  <SlMenu size={20} className='md:ml-2 hover:cursor-pointer' /> }
