75f67f2bf9cc6cbe195979f5e69a65cd2e1a9500
alanbacay02 <alan.social02@gmail.com>
2023-08-11 17:12:12 +0800

    Navbar redesign change. Changes login button, adds sign in button and navbar list items on lg devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
-import { CiUser } from 'react-icons/ci';
 import { SlMenu } from 'react-icons/sl';
 import MountainSVG from './MountainSVG.jsx';
 import PopoutMenu from './PopoutMenu.jsx';
@@ -24,28 +23,79 @@ const Navbar = () => {
       <div id='navbar' className='absolute top-0 left-0 right-0 flex flex-row justify-between items-center h-16 sm:h-20 md:h-24 w-full mx-auto px-2 md:px-6 text-white z-10'>
         <a href='/#'>
           <div className='flex flex-row items-center justify-center hover:cursor-pointer'>
-            <div className='w-[35px] xs:w-[50px] sm:w-[70px] h-full fill-white'>
+            <div className='w-[30px] xs:w-[50px] sm:w-[70px] h-full fill-white'>
               <MountainSVG />
             </div>
-            <h1 className='xs:text-xl sm:text-2xl md:text-3xl font-bold'>HIGH<span className='text-[#56ace1]'>LANDER</span>.</h1>
+            <h1 className='text-sm xs:text-lg sm:text-2xl md:text-3xl font-bold'>HIGH<span className='text-[#56ace1]'>LANDER</span>.</h1>
           </div>
         </a>
-        <div>
-          <div className='flex w-full flex-row justify-center items-center'>
-            <div
-            className='flex justify-center items-center p-[4px] sm:p-2 m-3 rounded-full bg-white text-black gap-1 hover:cursor-pointer'
-            onClick={handleLoginMenu} 
-            >
-              <CiUser className='text-xs xs:text-base sm:text-lg md:text-xl'/>
-              <p className='font-medium text-xs xs:text-base pr-1'>Log in</p>
-            </div>
-            <div className='block' onClick={handleNavMenu}>
-              {navMenu ? <AiOutlineClose size={20} className='md:ml-2 hover:cursor-pointer' /> :  <SlMenu size={20} className='md:ml-2 hover:cursor-pointer' /> }
-            </div>
+        
+        <div className='flex flex-row justify-center items-center'>
+          {/* Unordered List Items Shown On LG Devices */}
+          <div className='hidden lg:flex w-fit h-fit items-center mr-3'>
+            <ul className='flex flex-row justify-center items-center gap-10 xl:gap-14 text-lg'>
+              <li className='hover:cursor-pointer'>
+                <a href='/#' className='text-center group transition-all duration-300'>
+                  Home
+                  <span className='block max-w-0 group-hover:max-w-full transition-all ease-in-out duration-500 h-0.5 bg-white z-30' />
+                </a>
+              </li>
+              <li className='hover:cursor-pointer'>
+                <a href='/#' className='group transition-all duration-300'>
+                  About
+                  <span className='block max-w-0 group-hover:max-w-full ease-in-out duration-500 h-0.5 bg-white z-30' />
+                </a>
+              </li>
+              <li className='hover:cursor-pointer'>
+                <a href='/#' className='group transition-all duration-300'>
+                  Explore
+                  <span className='block max-w-0 group-hover:max-w-full transition-all ease-in-out duration-500 h-0.5 bg-white z-30' />
+                </a>
+              </li>
+              <li className='hover:cursor-pointer'>
+                <a href='/#' className='group transition-all duration-300'>
+                  Tickets
+                  <span className='block max-w-0 group-hover:max-w-full ease-in-out duration-500 h-0.5 bg-white z-30' />
+                </a>
+              </li>
+              <li className='hover:cursor-pointer'>
+                <a href='/#' className='group transition-all duration-300'>
+                  Hotels
+                  <span className='block max-w-0 group-hover:max-w-full transition-all ease-in-out duration-500 h-0.5 bg-white z-30' />
+                </a>
+              </li>
+            </ul>
           </div>
-        </div>
+          {/* End of Unordered List Items Shown On LG Devices */}
+          
+          {/* Start of Login Button */}
+          <div
+            className='group relative flex justify-center items-center p-1 xs:py-1 xs:px-2 sm:py-1 sm:px-6 ml-1 lg:ml-5 border-b-2 border-transparent hover:border-white hover:cursor-pointer hover:bg-transparent'
+            onClick={handleLoginMenu} 
+          >
+            <p className='font-medium text-[10px] xs:text-xs sm:text-lg'>Log in</p>
+            <div className='absolute bottom-0 h-0 w-full bg-blue-600 -z-10 group-hover:h-full ease-in-out duration-500' />
+          </div>
+          {/* End of Login Button */}
 
+          {/* Start of Signup Button */}
+          <div
+            className='group relative flex justify-center items-center p-1 xs:py-1 xs:px-2 sm:py-1 sm:px-6 ml-1 lg:ml-3 border-2 border-white gap-1 hover:cursor-pointer'
+            onClick={() => {}} 
+          >
+            <p className='font-medium text-[10px] xs:text-xs sm:text-lg'>Sign Up</p>
+            <div className='absolute inset-0 h-0 w-full bg-blue-600 -z-10 group-hover:h-full ease-in-out duration-500' />
+          </div>
+          {/* End of Signup Button */}
+
+          {/* Start of Hamburger Menu on Smaller Devices */}
+          <div className='block lg:hidden' onClick={handleNavMenu}>
+            {navMenu ? <AiOutlineClose className='text-base xs:text-lg sm:text-xl ml-2 hover:cursor-pointer' /> :  <SlMenu className='text-base xs:text-lg sm:text-2xl ml-2 sm:ml-4 hover:cursor-pointer' /> }
+          </div>
+          {/* End of Hamburger Menu on Smaller Devices */}
+        </div>
       </div>
+
       <PopoutMenu navMenu={navMenu} setNavMenu={setNavMenu} handleNavMenu={handleNavMenu} />
       <LoginMenu loginMenu={loginMenu} setLoginMenu={setLoginMenu} handleLoginMenu={handleLoginMenu} />
       {/* Overlay that appears behind `loginMenu` when active */}
