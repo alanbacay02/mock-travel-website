3747fc2554ec1f8975731a031350be181b55ab6e
alanbacay02 <alan.social02@gmail.com>
2023-09-12 15:38:11 +0800

    creates a component named AdventureCatalyst.jsx

---
+++ src/components/AdventureCatalyst.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ContourSVG2 } from '../svg-components/BackgroundSVG'
+
+const AdventureCatalyst = () => {
+  return (
+    <div className='relative w-full py-20 bg-stone-50 overflow-clip -z-50'>
+      <div className='w-full max-w-[800px] mx-auto'>
+        <div className='flex flex-col justify-center items-center text-center p-2 px-6'>
+          <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl mt-3 bg-stone-50'>Elate Your Adventure</h1>
+          <p className='mt-2 text-lg md:text-xl bg-stone-50'>Unleash your wanderlust with Highlander. We're your passport to extraordinary adventures. Explore, dream, and plan with ease. Start Planning now!</p>
+          <button className='px-2 py-2 sm:px-5 sm:py-3 md:px-6 md:py-3 mt-9 bg-blue-600 text-white font-bold text-xs md:text-lg'>Start Planning</button>
+        </div>
+      </div>
+      
+      <div className='absolute w-[2700px] h-[2000px] inset-0 -top-56 fill-gray-200 -z-40'>
+        <ContourSVG2 />
+      </div>
+    </div>
+  )
+}
+
+export default AdventureCatalyst
