2a514e2d88c354650f5f864c0b34fe5f33648089
alanbacay02 <alan.social02@gmail.com>
2023-09-13 15:27:11 +0800

    Makes buttons scrollable horizontally on small devices

--- src/components/ExploreOptions.jsx
+++ src/components/ExploreOptions.jsx
@@ -27,34 +27,52 @@ const BUTTON_INFO = [
   },
 ]
 
-const ExploreOptions = () => {
-  const onClickFunctions = [
-    () => {  // logic for the "Explore" button
-      return (
-        console.log('Explore button has been pressed')
-      )
-    },
-    () => {  // logic for the "Hotels" button
-      return (
-        console.log('Hotels button has been pressed')
-      )
-    },
-    () => {  // logic for the "Car Rent" button
-      // Car Rent button leads to a different page
-    },
-    () => {  // logic for the "Flights" button
-      // Flights button leads to a different page
-    },
-    () => {  // logic for the "Blogs" button
-      // Blogs button leads to a different page
-    },
-  ]
+const onClickFunctions = [
+  () => {  // logic for the "Explore" button
+    return (
+      console.log('Explore button has been pressed')
+    )
+  },
+  () => {  // logic for the "Hotels" button
+    return (
+      console.log('Hotels button has been pressed')
+    )
+  },
+  () => {  // logic for the "Car Rent" button
+    // Car Rent button leads to a different page
+  },
+  () => {  // logic for the "Flights" button
+    // Flights button leads to a different page
+  },
+  () => {  // logic for the "Blogs" button
+    // Blogs button leads to a different page
+  },
+]
 
+const ExploreOptions = () => {
   return (
     <div className='w-full py-7'>
       <div className='w-full mx-auto max-w-[1300px]'>
-
-        <div className='grid grid-cols-5 gap-7 px-6'>
+        <div className='scrolling-wrapper md:hidden pl-2'>
+          {BUTTON_INFO.map((items, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={onClickFunctions[index]}
+                  className='group relative rounded-2xl py-3 px-4 mr-2 border border-gray-400 hover:border-blue-600 transition-colors duration-500 font-medium overflow-clip'
+                >
+                  <div className='flex flex-row items-center justify-between gap-2 group-hover:text-white transition-colors duration-500'>
+                    <p>{items.buttonName}</p>
+                    <div className='text-xl'>
+                      {items.icon}
+                    </div>
+                  </div>
+                  <div className='absolute left-0 top-0 bottom-0 h-full -z-50 bg-blue-600 w-0 group-hover:w-full transition-all duration-500' />
+                </button>
+              )
+            })}
+        </div>
+        <div className='hidden md:grid grid-cols-5 gap-7 px-6'>
           {BUTTON_INFO.map((items, index) => {
             return (
               <button
